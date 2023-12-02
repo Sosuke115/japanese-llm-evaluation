d63de8bc7e491fdcf8330142c4a58b27beb74861
sosuke115 <sosuken115@gmail.com>
2023-12-02 17:30:24 +0900

    fix example

--- jrank/run_gen_model_answer_example.sh
+++ jrank/run_gen_model_answer_example.sh
@@ -1,14 +1,21 @@
-# model_name_or_path='studio-ousia/ELYZA-japanese-Llama-2-7b-8bit'
-adapter_path="/home/sosuke/qlora_ja/results/studio-ousia-ELYZA-japanese-Llama-2-7b-8bit-all/checkpoint-1000"
+#!/bin/bash
 
-python gen_model_answer.py \
-    --model-path $adapter_path \
-    --model-id "elyza-1000" \
-    --bench-name rakuda_v2 \
-    --conv_template llama2-ja \
-    --load_8bit \
-    --cpu_offloading \
-    --max_tokens 1024 \
-    --num_gpus 4 \
-    --debug
-    
+base_path="/home/sosuke/qlora_ja/results/studio-ousia-ELYZA-japanese-Llama-2-7b-8bit-all"
+
+for step in {1000..5000..1000}
+do
+    checkpoint_path="$base_path/checkpoint-$step"
+
+    python gen_model_answer.py \
+        --model-path $checkpoint_path \
+        --model-id "studio-ousia/elyza-checkpoint-$step" \
+        --bench-name rakuda_v2 \
+        --conv_template ./templates/llama2-ja.json \
+        --load_8bit \
+        --cpu_offloading \
+        --max_tokens 1024 \
+        --num_gpus 4 \
+        --debug
+
+    echo "Completed checkpoint $checkpoint"
+done
